fe1dbb7fec698ef40d4f89220370f61f52538190
Austin Osorio <osorio_austin@yahoo.com>
2020-01-16 15:46:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,5 +13,11 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
-
+  array = []
+  i = 0
+  while i < source_array.length do
+    array << source_array[i] * -1
+    i += 1
+  end
+  array
 end
