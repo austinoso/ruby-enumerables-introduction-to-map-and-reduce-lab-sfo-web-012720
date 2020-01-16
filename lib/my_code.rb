30515c90a74a16194b054260d3fd184cbbb128d8
Austin Osorio <osorio_austin@yahoo.com>
2020-01-16 15:42:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,6 +3,10 @@ def map_to_negativize(source_array)
   array = []
   i = 0
 
-  while i < source_array.length
+  while i < source_array.length do
+
+    array << source_array[i] * -1
+
+  end
 
 end
